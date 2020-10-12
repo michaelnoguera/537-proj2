4b1f6048202bb78d08a6889e44a432c7ab58f87a
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2020-10-12 18:51:15 -0500

    Update mock for testing Co-authored-by: Julien de Castelnau <decastelnau@wisc.edu>

--- mock.sh
+++ mock.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Mock of the program used for testing
 
-sed '/^.\{4096\}/d' | sed 's/ /\*/g' | sed 's/[a-z]/\U&/g'
-wc -l
+sed '/^.\{4096\}$/d' | sed 's/ /\*/g' | sed 's/[a-z]/\U&/g' | tee >(wc -l)
