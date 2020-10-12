5f9a710e1539b7abd77e7388f867762f1f299289
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2020-10-12 15:59:40 -0500

    Check return values of pthread operations in main.c

--- main.c
+++ main.c
@@ -18,30 +18,50 @@ int main() {
    pthread_t munch2_id;
    pthread_t writer_id;
 
-   Munch1Queue = CreateStringQueue(10);
-   Munch2Queue = CreateStringQueue(10);
-   WriteQueue = CreateStringQueue(10);
+   const int QUEUE_SIZE = 10;
+   Munch1Queue = CreateStringQueue(QUEUE_SIZE);
+   Munch2Queue = CreateStringQueue(QUEUE_SIZE);
+   WriteQueue = CreateStringQueue(QUEUE_SIZE);
 
-   /*
-   char* test = (char*)malloc(sizeof(char) * 16);
-   char* test2 = (char*)malloc(sizeof(char) * 16);
-   EnqueueString(Munch1Queue, test);
-   EnqueueString(Munch1Queue, test2);
-   printf("%s", DequeueString(Munch1Queue));
-   printf("%s", DequeueString(Munch1Queue));
-   */
+   // Spawn threads
+   if (pthread_create(&reader_id, NULL, Reader,NULL) != 0) {
+      perror("Error in Reader thread creation.");
+      exit(EXIT_FAILURE);
+   }
+   if (pthread_create(&munch1_id, NULL, Munch1,NULL) != 0) {
+      perror("Error in Munch1 thread creation.");
+      exit(EXIT_FAILURE);
+   }
+   if (pthread_create(&munch2_id, NULL, Munch2,NULL) != 0) {
+      perror("Error in Munch2 thread creation.");
+      exit(EXIT_FAILURE);
+   }
+   if (pthread_create(&writer_id, NULL, Writer,NULL) != 0) {
+      perror("Error in Writer thread creation.");
+      exit(EXIT_FAILURE);
+   }
 
-   pthread_create(&reader_id, NULL, Reader,NULL);
-   pthread_create(&munch1_id, NULL, Munch1,NULL);
-   pthread_create(&munch2_id, NULL, Munch2,NULL);
-   pthread_create(&writer_id, NULL, Writer,NULL);
+   // Merge threads
+   if (pthread_join(reader_id, NULL) != 0) {
+      perror("Error in Reader thread termination.");
+      exit(EXIT_FAILURE);
+   }
+   if (pthread_join(munch1_id, NULL) != 0) {
+      perror("Error in Munch1 thread termination.");
+      exit(EXIT_FAILURE);
+   }
+   if (pthread_join(munch2_id, NULL) != 0) {
+      perror("Error in Munch2 thread termination.");
+      exit(EXIT_FAILURE);
+   }
+   if (pthread_join(writer_id, NULL) != 0) {
+      // remember Writer prints total # lines upon termination
+      perror("Error in Writer thread termination.");
+      exit(EXIT_FAILURE);
+   }
 
-   pthread_join(reader_id, NULL);
-   pthread_join(munch1_id, NULL);
-   pthread_join(munch2_id, NULL);
-   pthread_join(writer_id, NULL);
-
-   fprintf(stderr, "\nSTATISTICS FOR Munch1Queue\n");
+   // Print statistics
+   fprintf(stderr, "STATISTICS FOR Munch1Queue\n");
    PrintQueueStats(Munch1Queue);
    fprintf(stderr, "STATISTICS FOR Munch2Queue\n");
    PrintQueueStats(Munch2Queue);
